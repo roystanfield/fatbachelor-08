ffd47be42a32c93a997f1e222abe94a4219bfd35
Roy Stanfield <roystanfield@gmail.com>
2014-12-19 16:42:57 -0500

    Initial Commit

---
+++ FatBachelor-08/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FatBachelor-08
+//
+//  Created by Roy Stanfield on 12/19/14.
+//  Copyright (c) 2014 Roy Stanfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FatBachelor-08/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FatBachelor-08
+//
+//  Created by Roy Stanfield on 12/19/14.
+//  Copyright (c) 2014 Roy Stanfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
